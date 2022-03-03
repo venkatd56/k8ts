706cf8b3714a0cf639b96e6cac7e0e8b59223fc4
davenkat <davenkatesh56@gmail.com>
2022-03-03 14:09:45 +0000

    14:New changes Added

---
+++ Labels/labels.sh
@@ -0,0 +1,5 @@
+#labels and Annotations
+
+Labels provide addional metdata information to the pod in the form of key value pairs, which helps to filter and selecting/quering the objects.
+
+Annotations provides more additional information that cannot be queried or selected more generic way we call it as documentation which gives more info regarding the object
